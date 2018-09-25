a213aa6e030366d87ea0eb60d0b51dbc2b36c725
Jesse Zhang <jzhang@pivotal.io>
2018-09-25 17:16:41 -0400

    Add a folder with Helpers for unittests
    Add a folder of Helper functions for unit tests in the same directory
    where the Mocks are stored
    
    Signed-off-by: Joao Pereira <jdealmeidapereira@pivotal.io>

---
+++ src/test/unit/helpers/elog_helper.c
@@ -0,0 +1,41 @@
+#include "elog_helper.h"
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmockery.h"
+
+#include "c.h"
+
+#include "postgres.h"
+
+#define PG_RE_THROW() siglongjmp(*PG_exception_stack, 1)
+
+/*
+ * This method will emulate the real ExceptionalCondition
+ * function by re-throwing the exception, essentially falling
+ * back to the next available PG_CATCH();
+ */
+void
+_ExceptionalCondition()
+{
+	PG_RE_THROW();
+}
+
+void
+expect_elog(int log_level)
+{
+	expect_any(elog_start, filename);
+	expect_any(elog_start, lineno);
+	expect_any(elog_start, funcname);
+	will_be_called(elog_start);
+	if (log_level < ERROR)
+		will_be_called(elog_finish);
+	else
+		will_be_called_with_sideeffect(elog_finish,
+		                               &_ExceptionalCondition,
+		                               NULL);
+
+	expect_value(elog_finish, elevel, log_level);
+	expect_any(elog_finish, fmt);
+}
---
+++ src/test/unit/helpers/elog_helper.h
@@ -0,0 +1,7 @@
+#ifndef GPDB_ELOG_HELPER_H
+#define GPDB_ELOG_HELPER_H
+
+void
+expect_elog(int log_level);
+
+#endif //GPDB_ELOG_HELPER_H
